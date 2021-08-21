c66b8612205fe592e6d984aa49a6ef88e146562e
Adrian <adrian.miranda.r@gmail.com>
2021-08-21 10:20:44 -0400

    Todas las vistas las rutas y metodos de crud

--- shows_app/urls.py
+++ shows_app/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.base),
     path('shows/', views.index),
     path('shows/new' , views.new),
+    path('shows/<int:id>' , views.shows),
+    path('shows/<int:id>/edit', views.edit),
+    path('shows/<int:id>/delete', views.delete)
+
 ]
--- shows_app/views.py
+++ shows_app/views.py
@@ -1,24 +1,68 @@
 from django.shortcuts import render , redirect
-from .models import Show
+from .models import *
 
 
 def base(request):
     return redirect('/shows')
-
-
 def index(request):
-    print(request.POST)
-    contexto = {
-        'titulo' : 'All Shows',
-        'programas' :  Show.objects.all()
-    }
-    if request.method == 'POST':
-        return redirect('/')
+    show = Show.objects.all()
     if request.method == 'GET':
+        contexto = {
+                'titulo' : 'All Shows',
+                'show' :  show
+            }
         return render(request, 'shows_app/index.html' , contexto)
+    if request.method == 'POST':
+        
+        print(request.POST)
+        
+        return redirect('/')
+
+def shows(request , id):
+    if request.method == 'GET':
+        contexto = {
+            'titulo'  : 'Information About Show',
+            'show' : Show.objects.get(id = id),
+        }
+        return render(request, 'shows_app/shows.html', contexto)
 
 def new(request):
-    contexto = {
-        'titulo' : 'New Program'
-    }
-    return render(request, 'shows_app/new.html', contexto)
+    if request.method == 'GET':
+        contexto = {
+            'titulo' : 'New Program'
+        }
+        return render(request, 'shows_app/new.html', contexto)
+
+    if request.method == 'POST':
+        print(request.POST)
+        show = Show.objects.create(
+            title = request.POST['title'],
+            network = request.POST['network'],
+            description = request.POST['description'],
+            release_date = request.POST['release_date']
+            )
+        return redirect(f'/shows/{show.id}')
+
+def edit(request, id):
+    show = Show.objects.get(id = id)
+    if request.method == 'GET':
+        contexto = {
+            'titulo' : f'Edit Show {show.id}',
+            'show' : show
+        }
+        return render(request , 'shows_app/edit.html', contexto)
+    
+    if request.method == 'POST':
+        print(request.POST , 'este es el post')
+        showActualizado = Show.objects.get(id = id)
+        showActualizado.title = request.POST['title']
+        showActualizado.network = request.POST['network']
+        showActualizado.description = request.POST['description']
+        showActualizado.release_date = request.POST['release_date'] 
+        showActualizado.save()
+        return redirect(f'/shows/{showActualizado.id}')
+
+def delete(request , id):
+    show = Show.objects.get(id = id)
+    show.delete()
+    return redirect('/')
